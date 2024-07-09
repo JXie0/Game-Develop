e46b00e362005cb31070c47917c2f57759029cad
JXie0 <sxie2566@andover.edu>
2024-07-09 11:09:21 -0400

    Update gameloop.js

--- gameloop.js
+++ gameloop.js
@@ -4,12 +4,14 @@ function gameLoop() {
   requestAnimationFrame (gameLoop);
 }
 function update() {
+  //Update game state
 }
 
 function render() {
+  //Draw game graphics
   ctx.clearRect(0,0, canvas.width,
 canvas.height);
-
+  //Example: draw a rectangle
   ctx.fillstyle = 'blue';
   ctx.fillRect(50, 50, 100, 100);
 }
